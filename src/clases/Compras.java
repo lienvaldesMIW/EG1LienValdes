7116be17b40e98dc996db9e230be6c8b6ccb40d0
lienvaldesMIW <lien.valdesi@alumnos.upm.es>
2013-10-30 16:18:59 +0100

    CD9

--- src/clases/Compras.java
+++ src/clases/Compras.java
@@ -7,9 +7,15 @@ public class Compras {
 	public int getTotal() {
 		return total;
 	}
-
 	public void setTotal(int total) {
 		this.total = total;
+	}
+
+	public Producto getProducto() {
+		return producto;
+	}
+	public void setProducto(Producto producto) {
+		this.producto = producto;
 	}
 	
 }
