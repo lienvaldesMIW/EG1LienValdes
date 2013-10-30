9ea576b142aa449037b7775ea12258476685da8d
lienvaldesMIW <lien.valdesi@alumnos.upm.es>
2013-10-30 16:04:05 +0100

    CR1.01

--- src/clases/Usuario.java
+++ src/clases/Usuario.java
@@ -2,13 +2,20 @@ package clases;
 
 public class Usuario {
 	private String nombre;
+	private String apellidos;
 
 	public String getNombre() {
 		return nombre;
 	}
-
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
 	}
 	
 }
