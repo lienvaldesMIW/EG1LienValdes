789c35151735d7483ea7dbba0631c2503a919804
lienvaldesMIW <lien.valdesi@alumnos.upm.es>
2013-10-30 16:15:27 +0100

    CR1.05

--- src/clases/Compras.java
+++ src/clases/Compras.java
@@ -1,5 +1,14 @@
 package clases;
 
 public class Compras {
+	private int total;
 
+	public int getTotal() {
+		return total;
+	}
+
+	public void setTotal(int total) {
+		this.total = total;
+	}
+	
 }
