187ab837d48f2d0aa8cc70d7b8a5172933aa1860
lienvaldesMIW <lien.valdesi@alumnos.upm.es>
2013-10-30 16:05:04 +0100

    CD5

--- src/clases/Producto.java
+++ src/clases/Producto.java
@@ -1,5 +1,14 @@
 package clases;
 
 public class Producto {
+	private String nombre;
 
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
 }
